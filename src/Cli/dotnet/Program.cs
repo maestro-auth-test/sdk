e8b42eb51ab93e0efa5a3d2dfbd76425c997cee3
Přemek Vysoký <premek.vysoky@microsoft.com>
2025-03-28 15:48:05 +0100

    Update Program.cs

--- src/Cli/dotnet/Program.cs
+++ src/Cli/dotnet/Program.cs
@@ -18,7 +18,7 @@ using Microsoft.DotNet.Cli.ShellShim;
 
 namespace Microsoft.DotNet.Cli;
 
-public class ProgramFromRepoAgain
+public class ProgramFromRepoAgain2
 {
     private static readonly string ToolPathSentinelFileName = $"{Product.Version}.toolpath.sentinel";
 
