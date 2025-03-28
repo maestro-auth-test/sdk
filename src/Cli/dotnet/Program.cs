ecb9ef401ac04d215478f3ecd6eafe1420ca662e
Premek Vysoky <premek.vysoky@microsoft.com>
2025-03-28 15:09:02 +0100

    Change class name

--- src/Cli/dotnet/Program.cs
+++ src/Cli/dotnet/Program.cs
@@ -18,7 +18,7 @@ using Microsoft.DotNet.Cli.ShellShim;
 
 namespace Microsoft.DotNet.Cli;
 
-public class Program
+public class ProgramFromRepo
 {
     private static readonly string ToolPathSentinelFileName = $"{Product.Version}.toolpath.sentinel";
 
