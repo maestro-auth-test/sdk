7bcf18b422519ed69619f39ba270f2bafe550199
Přemek Vysoký <premek.vysoky@microsoft.com>
2025-04-01 15:07:34 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Changing this in the repo dotnet/sdk
+
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   ScriptRoot="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
